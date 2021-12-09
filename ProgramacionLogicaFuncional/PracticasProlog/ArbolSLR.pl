df3e91d4c92a4ff5608768ccae3bcb4041127c90
Grecia Cortes <47579115+Nyoseka@users.noreply.github.com>
2021-12-09 15:53:28 -0800

    Create ArbolSLR.pl

---
+++ ProgramacionLogicaFuncional/PracticasProlog/ArbolSLR.pl
@@ -0,0 +1,10 @@
+mascota(Godzilla).
+mascota(Osa).
+nombre(Luis).
+nombre(Richy).
+nombre(Izai).
+trabajo(Botarga).
+trabajo(Gamer).
+trabajo(Programador).
+
+cuenta(X,Y,Z):- mascota(X),nombre(Y),trabajo(Z).
